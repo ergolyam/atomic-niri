d5d767c0852b3afff4f3d3d743eb4697b0aefc38
ergolyam <grisha765@tuta.io>
2025-10-28 19:07:36 +0300

    [f] disable gpg check

--- files/scripts/installproprietarypackages.sh
+++ files/scripts/installproprietarypackages.sh
@@ -5,6 +5,7 @@ set -oue pipefail
 
 curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
 sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+sed -i 's/gpgcheck=1/gpgcheck=0/' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
 rpm-ostree override replace \
   --experimental \
